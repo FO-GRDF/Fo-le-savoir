9fa224f13b86ea97fafb8f6f918a819fb2babd5d
Mickael Lopes <mickael.lopes1@gmail.com>
2026-09-15 10:48:59 +0200

    Embed Cour des comptes pour fo-grdf.fr

--- outils/embeds/build_embeds.py
+++ outils/embeds/build_embeds.py
@@ -13,7 +13,8 @@ TOOLS={
  'astreinte':'astreinte_disponibilite_permanente_fo_grdf.html',
  'catalogue-epi':'guide_epi_2026_fo_grdf.html',
  'consultation-ia-resultats':'resultats_consultation_ia_dsi_fo_grdf.html',
- 'calculette-trajet':'calculette_allongement_trajet_2026_fo_grdf.html',
+ 'cour-des-comptes':'saviez_vous_rapport_cour_comptes_edf_2026_fo_grdf.html',
+    'calculette-trajet':'calculette_allongement_trajet_2026_fo_grdf.html',
 }
 FONT_RE=re.compile(r'Fraunces|DM Sans|Atkinson Hyperlegible|DM\+Sans|Atkinson\+Hyperlegible')
 
